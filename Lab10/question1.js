fe0dfc5143a9d80758f71d77e3431eedb6783fc9
Siddhant Pageni <siddhantpageni@gmail.com>
2022-04-11 19:02:44 -0500

    Lab 10

---
+++ Lab10/question1.js
@@ -0,0 +1,9 @@
+Array.prototype.even = function(){
+    return this.filter(elem => (elem % 2) == 0);
+};
+Array.prototype.odd = function(){
+    return this.filter(elem => elem % 2 === 1);
+};
+
+console.log([1,2,3,4,5,6,7,8].even());
+console.log([1,2,3,4,5,6,7,8].odd());
